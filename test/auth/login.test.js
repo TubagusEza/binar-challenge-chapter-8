e8e2396b8c603cca3fbc205a4e773e146f0ddd9a
Tubagus Mochammad Eza <tubagusm.eza@gmail.com>
2022-06-14 21:43:52 +0700

    [REFACTOR] testing for login and register

--- test/auth/login.test.js
+++ test/auth/login.test.js
@@ -1,13 +1,23 @@
 const request = require('supertest');
 const app = require('../../app');
 
-describe('User', () => {
+describe('POST /v1/auth/login', () => {
   it('Login user', () => request(app)
     .post('/v1/auth/login')
     .set('Accept', 'application/json')
     .send({
-      email: 'tubagus@gmail.com',
-      password: 'tubagus',
+      email: 'fikri@binar.co.id',
+      password: '123456',
     })
     .expect(201));
+});
+it('Login user which is not registered', () => {
+  return request(app)
+    .post('/v1/auth/login')
+    .set('Accept', 'application/json')
+    .send({
+      email: 'fikri2@binar.co.id',
+      password: '123456',
+    })
+    .expect(404);
 });
--- test/auth/register.test.js
+++ test/auth/register.test.js
@@ -1,16 +1,36 @@
 const request = require('supertest');
 const app = require('../../app');
+const { User } = require('../../app/models');
 
 describe('User', () => {
+  let user;
+  afterAll(async () => {
+    user = await User.destroy({
+      where: {
+        email: 'tubagus@gmail.com',
+      },
+    });
+  });
   it('Register user', () => {
     return request(app)
       .post('/v1/auth/register')
       .set('Accept', 'application/json')
       .send({
-        name: 'yuneda',
-        email: 'yuneda3@gmail.com',
-        password: 'yuneda',
+        name: 'tubagus',
+        email: 'tubagus@gmail.com',
+        password: 'tubagus',
       })
       .expect(201);
+  });
+  it('Register user with same email', () => {
+    return request(app)
+      .post('/v1/auth/register')
+      .set('Accept', 'application/json')
+      .send({
+        name: 'eza',
+        email: 'tubagus@gmail.com',
+        password: 'eza',
+      })
+      .expect(422);
   });
 });
